0def326af4d40e5164e5b43ad6003770f9235d02
EastHyeon <99802932+EastHyeon@users.noreply.github.com>
2024-03-21 15:00:11 +0900

    Feat: Print image more faster

--- GhwaJaeEngine/Lecture03-HW/Lecture03-HW.cpp
+++ GhwaJaeEngine/Lecture03-HW/Lecture03-HW.cpp
@@ -19,7 +19,8 @@ enum ForeColor {
     WHITE = 37,
 };
 #pragma region 수상한 코드
-const char* ContrastMap = " .,'`^:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
+// .,'`^:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$
+const char* ContrastMap = "@";
 
 typedef struct {
     enum ForeColor colorTag;
@@ -40,6 +41,9 @@ DefineColor ConsoleColors[] = {
     {ForeColor::WHITE, 255, 255, 255}
 };
 
+void PrintImageFast(CImg<unsigned char> origin);
+void PrintImage(CImg<unsigned char> origin);
+
 DefineColor FindColorByColorCode(ForeColor colorCode) 
 {
     for (int i = 0; i < sizeof(ConsoleColors) / sizeof(ConsoleColors[0]); i++)
@@ -50,7 +54,55 @@ DefineColor FindColorByColorCode(ForeColor colorCode)
     return ConsoleColors[0];
 }
 
-void PrintImageWithColor(CImg<unsigned char> grayWeight, ForeColor colorCode) {
+CImg<unsigned char> GetGrayScaleImage(CImg<unsigned char> origin)
+{
+    CImg<unsigned char> grayWeight(origin.width(), origin.height(), 1, 1, 0);
+
+    cimg_forXY(origin, x, y) {
+        int R = static_cast<int>(origin(x, y, 0, 0));
+        int G = static_cast<int>(origin(x, y, 0, 1));
+        int B = static_cast<int>(origin(x, y, 0, 2));
+
+        // Luma coding을 이용한 grayscale 변환법
+        // 밝기 정보(Luma)만을 추출하여 grayscale 이미지를 생성한다.
+        // 인간의 시각이 가장 민감한 녹색을 크게 반영하여 계산한다.
+        int grayValueWeight = (int)(0.299 * R + 0.587 * G + 0.114 * B);
+
+        grayWeight(x, y, 0, 0) = grayValueWeight;
+    }
+    
+    return grayWeight;
+}
+
+CImg<unsigned char> InitializeImage(const char* address, int targetWidth) {
+    CImg<unsigned char> origin(address);
+
+    float ratio = (float)origin.height() / (float)origin.width();
+    int resizedHeight = static_cast<int>(targetWidth * ratio);
+    int resizedWidth = targetWidth * 2;
+
+    origin.resize(resizedWidth, resizedHeight);
+
+    return origin;
+}
+
+CImg<unsigned char> GetSourceImage(const char* address, int targetWidth) {
+    CImg<unsigned char> origin(address);
+
+    float ratio = (float)origin.height() / (float)origin.width();
+    int resizedHeight = static_cast<int>(targetWidth * ratio);
+    int resizedWidth = targetWidth * 2;
+
+    origin.resize(resizedWidth, resizedHeight);
+
+    PrintImageFast(origin);
+
+    return origin;
+}
+
+void PrintImageWithColor(CImg<unsigned char> origin, ForeColor colorCode) {
+    CImg<unsigned char> grayWeight = GetGrayScaleImage(origin);
+
     CImg<unsigned char> colored(grayWeight.width(), grayWeight.height(), 1, 3, 0);
 
     cimg_forXY(grayWeight, x, y) {
@@ -62,77 +114,89 @@ void PrintImageWithColor(CImg<unsigned char> grayWeight, ForeColor colorCode) {
         colored(x, y, 0, 1) = GRAY * (defineColor.g) / 256.0f * 256.0f;
         colored(x, y, 0, 2) = GRAY * (defineColor.b) / 256.0f * 256.0f;
     }
-    for (int y = 0; y < colored.height(); y++)
+    //7 10 11 14 15 18 19
+    // 색상 확장 시퀀스 문자열 길이: 19
+    PrintImageFast(colored);
+}
+
+void PrintImageFast(CImg<unsigned char> origin) 
+{
+    CImg<unsigned char> grayWeight = GetGrayScaleImage(origin);
+
+    //7 10 11 14 15 18 19
+    // 색상 확장 시퀀스 문자열 길이: 19
+    const int PIXEL_SPACE = 20;
+
+    const int BUFFER_WITDH_SIZE = origin.width() * PIXEL_SPACE + 1;
+
+    char* buffer = static_cast<char*>(malloc(sizeof(char) * BUFFER_WITDH_SIZE * origin.height()));
+
+    for (int y = 0; y < origin.height(); y++)
     {
-        for (int x = 0; x < colored.width(); x++)
+        for (int x = 0; x < origin.width(); x++)
         {
-            unsigned char R = colored(x, y, 0, 0);
-            unsigned char G = colored(x, y, 0, 1);
-            unsigned char B = colored(x, y, 0, 2);
+            int pixelSizedX = x * PIXEL_SPACE;
+
+            unsigned char R = origin(x, y, 0, 0);
+            unsigned char G = origin(x, y, 0, 1);
+            unsigned char B = origin(x, y, 0, 2);
 
             int GRAY = static_cast<int>(grayWeight(x, y, 0, 0));
 
             int index = static_cast<int>(static_cast<int>(GRAY) / 256.0f * strlen(ContrastMap));
 
-            cout << "\x1b[38;2;" << static_cast<int>(R) << ";" << static_cast<int>(G) << ";" << static_cast<int>(B) << "m" << ContrastMap[index];
+            int count = 0;
+            buffer[y * (BUFFER_WITDH_SIZE)+(pixelSizedX + count++)] = '\x1b';
+            buffer[y * (BUFFER_WITDH_SIZE)+(pixelSizedX + count++)] = '[';
+            buffer[y * (BUFFER_WITDH_SIZE)+(pixelSizedX + count++)] = '3';
+            buffer[y * (BUFFER_WITDH_SIZE)+(pixelSizedX + count++)] = '8';
+            buffer[y * (BUFFER_WITDH_SIZE)+(pixelSizedX + count++)] = ';';
+            buffer[y * (BUFFER_WITDH_SIZE)+(pixelSizedX + count++)] = '2';
+            buffer[y * (BUFFER_WITDH_SIZE)+(pixelSizedX + count++)] = ';';
+
+            char colorBuffer[4];
+
+            sprintf_s(colorBuffer, "%3d", static_cast<int>(R));
+            for (int i = 0; i < 3; i++)
+            {
+                if (colorBuffer[i] == ' ')
+                    buffer[y * (BUFFER_WITDH_SIZE)+(pixelSizedX + count++)] = '0';
+                else
+                    buffer[y * (BUFFER_WITDH_SIZE)+(pixelSizedX + count++)] = colorBuffer[i];
+            }
+            buffer[y * (BUFFER_WITDH_SIZE)+(pixelSizedX + count++)] = ';';
+
+            sprintf_s(colorBuffer, "%3d", static_cast<int>(G));
+            for (int i = 0; i < 3; i++)
+            {
+                if (colorBuffer[i] == ' ')
+                    buffer[y * (BUFFER_WITDH_SIZE)+(pixelSizedX + count++)] = '0';
+                else
+                    buffer[y * (BUFFER_WITDH_SIZE)+(pixelSizedX + count++)] = colorBuffer[i];
+            }
+            buffer[y * (BUFFER_WITDH_SIZE)+(pixelSizedX + count++)] = ';';
+
+            sprintf_s(colorBuffer, "%3d", static_cast<int>(B));
+            for (int i = 0; i < 3; i++)
+            {
+                if (colorBuffer[i] == ' ')
+                    buffer[y * (BUFFER_WITDH_SIZE)+(pixelSizedX + count++)] = '0';
+                else
+                    buffer[y * (BUFFER_WITDH_SIZE)+(pixelSizedX + count++)] = colorBuffer[i];
+            }
+
+            buffer[y * (BUFFER_WITDH_SIZE)+(pixelSizedX + count++)] = 'm';
+            buffer[y * (BUFFER_WITDH_SIZE)+(pixelSizedX + count++)] = ContrastMap[index];
         }
-        cout << "\n";
+        buffer[y * BUFFER_WITDH_SIZE + BUFFER_WITDH_SIZE - 1] = '\n';
     }
+    buffer[origin.height() * BUFFER_WITDH_SIZE - 1] = '\0';
+    cout << buffer << endl;
 }
 
-CImg<unsigned char> InitializeImage(const char* address) {
-    CImg<unsigned char> origin(address);
-
-    float ratio = (float)origin.height() / (float)origin.width();
-    int targetWidth = 50;
-    int resizedHeight = static_cast<int>(targetWidth * ratio);
-    int resizedWidth = targetWidth * 2;
-
-    origin.resize(resizedWidth, resizedHeight);
-
-    CImg<unsigned char> grayWeight(origin.width(), origin.height(), 1, 1, 0);
-
-
-    cimg_forXY(origin, x, y) {
-        int R = static_cast<int>(origin(x, y, 0, 0));
-        int G = static_cast<int>(origin(x, y, 0, 1));
-        int B = static_cast<int>(origin(x, y, 0, 2));
-
-        // Luma coding을 이용한 grayscale 변환법
-        // 밝기 정보(Luma)만을 추출하여 grayscale 이미지를 생성한다.
-        // 인간의 시각이 가장 민감한 녹색을 크게 반영하여 계산한다.
-        int grayValueWeight = (int)(0.299 * R + 0.587 * G + 0.114 * B);
-
-        grayWeight(x, y, 0, 0) = grayValueWeight;
-    }
-    return grayWeight;
-}
-
-CImg<unsigned char> GetSourceImage(const char* address) {
-    CImg<unsigned char> origin(address);
-
-    float ratio = (float)origin.height() / (float)origin.width();
-    int targetWidth = 50;
-    int resizedHeight = static_cast<int>(targetWidth * ratio);
-    int resizedWidth = targetWidth * 2;
-
-    origin.resize(resizedWidth, resizedHeight);
-
-    CImg<unsigned char> grayWeight(origin.width(), origin.height(), 1, 1, 0);
-
-
-    cimg_forXY(origin, x, y) {
-        int R = static_cast<int>(origin(x, y, 0, 0));
-        int G = static_cast<int>(origin(x, y, 0, 1));
-        int B = static_cast<int>(origin(x, y, 0, 2));
-
-        // Luma coding을 이용한 grayscale 변환법
-        // 밝기 정보(Luma)만을 추출하여 grayscale 이미지를 생성한다.
-        // 인간의 시각이 가장 민감한 녹색을 크게 반영하여 계산한다.
-        int grayValueWeight = (int)(0.299 * R + 0.587 * G + 0.114 * B);
-
-        grayWeight(x, y, 0, 0) = grayValueWeight;
-    }
+void PrintImage(CImg<unsigned char> origin) 
+{
+    CImg<unsigned char> grayWeight = GetGrayScaleImage(origin);
 
     for (int y = 0; y < origin.height(); y++)
     {
@@ -151,16 +215,40 @@ CImg<unsigned char> GetSourceImage(const char* address) {
         }
         cout << "\n";
     }
-
-    return grayWeight;
 }
+
 #pragma endregion
 
 int main()
 {
 #pragma region 수상한 코드
-    CImg<unsigned char> source = InitializeImage("marioblock.jpg");
+    const int TARGET_WIDTH = 200;
+    CImg<unsigned char> source = InitializeImage("marioblock.jpg", TARGET_WIDTH);
 #pragma endregion
+
+    const int MAX_FRAME = 299;
+    int frame = 0;
+    char frameCharBuffer[4];
+
+
+    char buffer[1000];
+    while (true)
+    {
+        sprintf_s(frameCharBuffer, "%3d", frame);
+        for (int i = 0; i < 3; i++)
+        {
+            if (frameCharBuffer[i] == ' ')
+                frameCharBuffer[i] = '0';
+        }
+        cout << "\x1b[0;0H";
+        sprintf_s(buffer, "file_example_MP4_480_1_5MG_000\\file_example_MP4_480_1_5MG_%s.jpg", frameCharBuffer);
+        source = GetSourceImage(buffer, TARGET_WIDTH);
+        frame++;
+        Sleep(10);
+        if (frame >= MAX_FRAME)
+            frame = 0;
+    }
+
     cout << "화면에 그림을 그리는 프로그램입니다." << endl;
     cout << "학번 : 202327005" << endl;
     cout << "이름 : 김동현" << endl;
@@ -170,40 +258,39 @@ int main()
         int input;
         cin >> input;
 
-        if (input <= 64 || input == 100 || input == 101) {
-            ForeColor selectedColor = ForeColor::DEFAULT;
-#pragma region 수상한 코드
-            if (input == 100) {
-                source = GetSourceImage("professor_es.jpg");
-                continue;
-            }
-            if (input == 101) {
-                source = GetSourceImage("marioblock.jpg");
-                continue;
-            }
-#pragma endregion
-            if (input & 0b1) {
-                PrintImageWithColor(source, ForeColor::WHITE);
-            }
-            if (input & 0b10) {
-                PrintImageWithColor(source, ForeColor::RED);
-            }
-            if (input & 0b100) {
-                PrintImageWithColor(source, ForeColor::GREEN);
-            }
-            if (input & 0b1000) {
-                PrintImageWithColor(source, ForeColor::YELLOW);
-            }
-            if (input & 0b10000) {
-                PrintImageWithColor(source, ForeColor::CYAN);
-            }
-            if (input & 0b100000) {
-                PrintImageWithColor(source, ForeColor::MAGENTA);
-            }
-            if (input == 64)
-                break;
-        }
-        else
+        if (input > 64 && input != 100 && input != 101) {
             cout << "올바른 코드를 입력하십시오." << endl;
+            continue;
+        }
+#pragma region 수상한 코드
+        if (input == 100) {
+            source = GetSourceImage("professor_es.jpg", TARGET_WIDTH);
+            continue;
+        }
+        if (input == 101) {
+            source = GetSourceImage("marioblock.jpg", TARGET_WIDTH);
+            continue;
+        }
+#pragma endregion
+        if (input & 0b1) {
+            PrintImageWithColor(source, ForeColor::WHITE);
+        }
+        if (input & 0b10) {
+            PrintImageWithColor(source, ForeColor::RED);
+        }
+        if (input & 0b100) {
+            PrintImageWithColor(source, ForeColor::GREEN);
+        }
+        if (input & 0b1000) {
+            PrintImageWithColor(source, ForeColor::YELLOW);
+        }
+        if (input & 0b10000) {
+            PrintImageWithColor(source, ForeColor::CYAN);
+        }
+        if (input & 0b100000) {
+            PrintImageWithColor(source, ForeColor::MAGENTA);
+        }
+        if (input == 64)
+            break;
     }
 }
