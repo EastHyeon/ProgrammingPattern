78a9d0f27718e9eacc3c93a4a727b3f84fb0b457
EastHyeon <easthyeon04@gmail.com>
2024-03-21 21:43:47 +0900

    Create ASCII_MediaPlayer and add FFmpeg, CImg

---
+++ GhwaJaeEngine/ASCII_MediaPlayer/ASCII_MediaPlayer.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+// CImg
+#pragma warning(disable:4146)
+#define cimg_use_jpeg
+#include "CImg.h"
+using namespace cimg_library;
+using namespace std;
+
+// FFmpeg
+extern "C"{
+#include "libavcodec/avcodec.h"
+#include "libavdevice/avdevice.h"
+#include "libavfilter/avfilter.h"
+#include "libavformat/avformat.h"
+#include "libavutil/imgutils.h"
+#include "libpostproc/postprocess.h"
+#include "libswresample/swresample.h"
+#include "libswscale/swscale.h"
+}
+#pragma comment(lib, "avcodec.lib")
+#pragma comment(lib, "avdevice.lib")
+#pragma comment(lib, "avfilter.lib")
+#pragma comment(lib, "avformat.lib")
+#pragma comment(lib, "avutil.lib")
+#pragma comment(lib, "postproc.lib")
+#pragma comment(lib, "swresample.lib")
+#pragma comment(lib, "swscale.lib")
+#pragma comment(lib, "avutil-58.dll")
+
+AVFormatContext* fmtCtx;
+
+int main(void) {
+
+    av_log(NULL, AV_LOG_INFO, "AVFromat major version = %d\n", LIBAVFORMAT_VERSION_MAJOR);
+
+    return 0;
+
+}
